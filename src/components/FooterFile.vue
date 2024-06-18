e7a0f87d69c807cfb6ac1c4ce4abbbe72ffbe7b5
gioudi <sergiopenagos881@gmail.com>
2024-06-18 04:31:33 -0500

    SCRUM-36-Update-the-UI-to-and-ensure-is-working-based-on-the-custom-grid

--- src/components/FooterFile.vue
+++ src/components/FooterFile.vue
@@ -1,33 +1,6 @@
 <template>
   <footer class="footer">
     <p>&copy; Sergio Penagos All rights reserved.</p>
-    <div
-      class="contact-options is-flex is-justify-content-center"
-      data-aos="fade-left"
-    >
-      <a :href="'mailto:' + email" title="Email" data-aos="flip-right"
-        ><span class="icon is-large is-hoverable is-size-3"
-          ><i class="fa fa-envelope"></i></span
-      ></a>
-      <a
-        href="https://www.linkedin.com/in/analyst-sergio-penagos/"
-        target="_blank"
-        title="linkedin"
-        data-aos="flip-right"
-      >
-        <span class="icon is-large is-hoverable is-size-3">
-          <i class="fab fa-linkedin"></i>
-        </span>
-      </a>
-      <a :href="github" target="_blank" title="GitHub" data-aos="flip-right"
-        ><span class="icon is-large is-hoverable is-size-3"
-          ><i class="fab fa-github"></i></span
-      ></a>
-      <a :href="github2" target="_blank" title="GitHub" data-aos="flip-right"
-        ><span class="icon is-large is-hoverable is-size-3"
-          ><i class="fab fa-github"></i></span
-      ></a>
-    </div>
   </footer>
 </template>
 
@@ -36,16 +9,5 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "FooterFile",
-  setup() {
-    const email = "sergiopenagos881@gmail.com";
-    const github = "https://github.com/gioudi";
-    const github2 = "https://github.com/SergioVass";
-
-    return {
-      email,
-      github,
-      github2,
-    };
-  },
 });
 </script>
