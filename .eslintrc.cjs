fd8a383bf7d1c5c5d1e268c059adff3d85684e0b
gioudi <sergiopenagos881@gmail.com>
2026-08-31 16:18:04 -0500

    test(vitest): port 8 unit tests from jest to vitest; eslintrc to cjs (SPEC-P2-02)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,26 +2,27 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
     ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "warn",
+    "no-debugger": "warn",
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ["**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
         jest: true,
       },
